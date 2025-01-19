82644948e7103f5d39237c4cb3a27be4c92b4bcf
Nattaya-ati <6752g10001@sau.ac.th>
2025-01-19 16:50:00 +0700

    V.1 Create MyDTISAU02Project

---
+++ src/mydti02project/MyDTI02Project.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mydti02project;
+
+/**
+ *
+ * @author 66650
+ */
+public class MyDTI02Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
